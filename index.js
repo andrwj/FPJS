3e864716535f941b0fc6bf1f25eb6e4abdc7854a
A.J <andrwj@gmail.com>
2019-08-07 06:31:41 +0900

    개발 디버깅할 때 loader.js를 사용하도록 전체 수정

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-module.exports = require('esm')(module, { cache: false })('./FP.js');
+export { default as reduce } from './reduce.js';
+export { default as run } from './run.js';
+export { default as pipe } from './pipe.js';
+export { Maybe, Just, Nothing, lift } from './maybe.js';
+export { Either, identity, revoke, truth, isUndefined } from './either.js';
+export { default as list } from './list.js';
+export { isIterable, ternary, sleep, mixin, attach } from './utils.js';
+export { default as Fromise } from './fromise.js';
+export { Any, All } from './semigroup.js';
+export { default as isFunction } from './isFunction.js';
+export { isFalsy, isNotFalsy } from './isFalsy.js';
+export { default as isFalse } from './isFalse.js';
+export { __true, __false } from './true-false.js';
