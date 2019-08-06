464adbe1b8e9ef5ae986464f3cdb15ee29d17bc2
A.J <andrwj@gmail.com>
2019-08-07 06:30:46 +0900

    throw/catch, of 정리

--- either.js
+++ either.js
@@ -5,6 +5,7 @@ export const identity = v => v;
 export const revoke = () => undefined;
 export const truth = v => !!v;
 export const isUndefined = v => Object.is(v, undefined);
+export const getOrElse = (value, if_undefined) => isUndefined(value) ? if_undefined : value;
 
 export class Either {
   constructor(args) {
@@ -28,9 +29,9 @@ export class Either {
 
   ['catch'] () {return this;}
 
-  ['clone'] () {return this;}
-
   ['apply'] () {return this;}
+
+  clone () {return this;}
 
   tap (f=console.log) {f(this.value); return this;}
 
@@ -53,55 +54,104 @@ export class Either {
   throwIf () { return this; }
 };
 
-Either.of = (v, f) =>  isFunction(f) ? (f(v) ? Either.right(v) : Either.left(v)) : Either.right(v);
-Either.fromNullable = v => Either.of(v, truth);
-Either.filter = (f, cond=truth) => {
-  const v = f();
-  return cond(v) ? Either.right(v) : Either.left(v);
-};
-Either.right = R.curry(v => new Right(v));
-Either.left = R.curry(v => new Left(v));
-Either.done = v => new Done(Either.right(v));
-Either.doneIf = (f, v) => {return f(v) ? Either.done(v) : Either.right(v); };
-Either.throw = v => new Throw(Either.right(v));
-Either.throwIf = (f, v) => f(v) ? new Throw(Either.right(v)) : Either.right(v);
-Either.try = (f)  => {
-  try {return Either.right(f());} catch(e) {return Either.left(e);}
+Either.of = (v, f, if_true=Either.right, if_false=Either.left) => {
+  if( !isFunction(f) ) return Either.right(v);
+  return f(v) ? if_true(v) : if_false(v);
 };
 
+Either.fromNullable = v => Either.of(v, truth);
+
+Either.filter = (f, judge=truth, if_true=Either.right, if_false=Either.false) => {
+  const v = f();
+  return judge(v) ? if_true(v) : if_false(v);
+};
+
+Either.right = R.curry( v => new Right(v) );
+
+Either.left = R.curry( v => new Left(v) );
+
+/* only '.take()' or '.fold()' handles instance of 'Done' */
+Either.done = (v, functor=Either.right) => new Done( functor(v) );
+
+Either.doneIf = (f, v, functor=Either.right) => { return f(v) ? Either.done(v, functor) : functor(v); };
+
+/* 'v' must be Right or Left. If it is not of Either, Right(v) will be passed. */
+Either.throw = v => new Throw( (v instanceof Either) ? v : Either.right(v) );
+
+/* `throw` does not mean "throwing Error" but "to carrying Right or Left" */
+Either.throwIf = (f, v, if_true=Either.right, if_false=Either.left) => new Throw(f(v) ? if_true(v) : if_false(v));
+
+/* WARNING: when 'f' contains Promise, beware to have '.catch()' on Promise.
+ * Or Be sure to use 'await' to avoid the Error "Unhandled Promise Rejection" */
+Either.try = (f)  => {
+  try {
+    return Either.right(f());
+  } catch(e) {
+    return Either.left(e);
+  }
+};
 
 class Right extends Either {
 
-  constructor(args) {
-    super(args);
+  of (v, f, if_true=Either.right, if_false=Either.left) {
+    /* use this when you want to change the instance's value */
+    if( !isFunction(f) ) return Either.right(v);
 
+    /* if 'f' is given */
+    return f(v) ? Either.right(v) : Either.left(v);
   }
 
-  ['try'] (f) { try {return Either.right(f(this.value));} catch(e) {return Either.left(e);} }
-
-  ['throw'] () {return Either.throw(this.value);}
-
-  ['clone'] (v) {return Either.right(isUndefined(v) ? this.value : v);}
-
-  ['apply'] (o) {return o.map(this.value);}
-
-  of (v, f) {return isFunction(f) ? (f(v) ? Either.right(v) : Either.left(v)) : Either.right(v);}
+  clone (v) {return Either.right( getOrElse(v, this.value) );}
 
   take () {return this.value;}
 
-  map (f) {return Either.right(f(this.value));}
+  map (f) {return Either.right( f(this.value) );}
 
   chain (f) {return f(this.value);}
 
-  filter (f) { return Either.of(this.value, f);}
+  filter (f) {return Either.of( this.value, f );}
 
-  fold (f=identity, no_use) {return f(this.value);}
+  fold ( /* for Right */f=identity /* for Left, but no_use */) { return f(this.value); }
 
   done (v) { return Either.done(isUndefined(v) ? this.value : v); }
 
-  doneIf (f, v) {return f(this.value) ? Either.done(isUndefined(v) ? this.value : v) : this;}
+  doneIf (f, v) { return f(this.value) ? Either.done( getOrElse(v, this.value) ) : this;}
 
-  throwIf (f, v) { return f(this.value) ? Either.throw(isUndefined(v) ? this.value : v) : this; }
+  /* remember that we not pass 'Error' nor 'Exception', but Right or Left.
+   * if 'v' is supplied, it will be thrown to '.catch()' instead of instance's value
+   * this is useful when we want to pass some other value based on condition.
+   */
+  throwIf (f, v, if_true=Either.right) {
+    return f(this.value) ? Either.throw( if_true(getOrElse(v, this.value)) ) : this;
+  }
+
+  ['try'] (f) {
+    try {
+      return Either.right(
+        /* if f() returns Promise, be careful to not get 'Unhandled Promise Rejection'
+         * you have to attach '.catch()' on Promise when you defined inside f().
+         * This is because async function returns Promise always.
+         * So this 'try' definition can not be declared as 'async' function.
+         */
+        f(this.value)
+      );
+    } catch(e) {
+      return Either.left(e);
+    }
+  }
+
+  ['throw'] (f) {
+    /* optional 'f' could be used before throwing. (e.g notifying, logging..) */
+    if( isFunction(f) ) f(this.value);
+
+    /* since only Right can throw, passing value must be of Right */
+    return Either.throw( Either.right(this.value) );
+  }
+
+  ['apply'] (o) {
+    return o.map(this.value);
+  }
+
 }
 
 // Class Left
@@ -109,7 +159,6 @@ class Left extends Either {
 
   constructor(args) {
     super(args);
-
   }
 
   take () {return this.value;}
@@ -122,13 +171,14 @@ class Left extends Either {
 // NOTE: The return value must be of Right or the caller of '.throw()'
 class Throw extends Either {
   constructor(args) {
-    if(!(args instanceof Either)) throw new Error(`Throw() needs instance of Either`);
+    if(!(args instanceof Either)) throw new Error(`Throw(): argument must be of Either`);
     super(args);
-
   }
 
   ['catch'] (handler) {
-    return isFunction(handler) ? this.value.map(handler) : this.value;
+    /* WARNING: Be careful in handler. If 'handler' is omitted, there will be no problem.
+     * but if it is given, it must return Right or Left. Otherwiase, chain will be broken. */
+    return isFunction(handler) ? handler(this.value) : this.value;
   }
 
   tap (f=console.log) {
@@ -148,16 +198,16 @@ class Throw extends Either {
 
 }
 
-// NOTE: Done is another carrier for Right and Left. Only instance of Done will ignore all method but '.take() & .fold()'
+/* NOTE: Done is another carrier for Right and Left.
+ * Only instance of Done will ignore all method but '.take() & .fold()' */
 class Done extends Either {
 
   constructor(args) {
-    if(!(args instanceof Either)) throw new Error(`argument must be of Either`);
+    if(!(args instanceof Either)) throw new Error(`Done(): argument must be of Either`);
     super(args);
 
   }
 
-  // for DEBUGGING
   tap (f=console.log) {
     (function recur(obj) {
       return (obj && (obj instanceof Either) && (obj.value instanceof Either)) ? recur(obj.value) : f(obj.value);
